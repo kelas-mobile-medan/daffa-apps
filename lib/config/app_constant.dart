7f54889f6e6ca03f5a0c932f5da94242fc2205da
DaffaAfdallah <daffaafdallah11@gmail.com>
2024-06-18 23:03:56 +0700

    masih error ;)

--- lib/config/app_constant.dart
+++
@@ -1,3 +0,0 @@
-class AppConstant {
-  static const String baseUrl = 'https://reqres.in';
-}
--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,54 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:circlestyle/api/auth_services.dart'; // Import AuthServices
-import 'package:circlestyle/screens/home_screen.dart';
-import 'package:circlestyle/screens/sign_up_screen.dart';
-import 'package:circlestyle/screens/forgot_password_screen.dart';
+import 'sign_up_screen.dart';
+import 'forgot_password_screen.dart';
+import 'home_screen.dart';
 
-class SignInScreen extends StatefulWidget {
+class SignInScreen extends StatelessWidget {
   static const routeName = '/sign-in';
-
-  @override
-  _SignInScreenState createState() => _SignInScreenState();
-}
-
-class _SignInScreenState extends State<SignInScreen> {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final AuthServices authServices = AuthServices();
-
-  void _login() async {
-    String email = emailController.text;
-    String password = passwordController.text;
-
-    try {
-      var authModel = await authServices.login(email, password);
-      if (authModel.token != null) {
-        Navigator.of(context).pushReplacementNamed(HomePage.routeName);
-      } else {
-        _showErrorDialog('Invalid credentials');
-      }
-    } catch (e) {
-      _showErrorDialog(e.toString());
-    }
-  }
-
-  void _showErrorDialog(String message) {
-    showDialog(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: Text('Login Failed'),
-        content: Text(message),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(ctx).pop();
-            },
-            child: Text('Okay'),
-          ),
-        ],
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,7 +31,6 @@ class _SignInScreenState extends State<SignInScreen> {
               ),
               SizedBox(height: 16),
               TextField(
-                controller: emailController,
                 decoration: InputDecoration(
                   labelText: 'Username or Email',
                   prefixIcon: Icon(Icons.person),
@@ -86,7 +41,6 @@ class _SignInScreenState extends State<SignInScreen> {
               ),
               SizedBox(height: 16),
               TextField(
-                controller: passwordController,
                 decoration: InputDecoration(
                   labelText: 'Password',
                   prefixIcon: Icon(Icons.lock),
@@ -117,7 +71,10 @@ class _SignInScreenState extends State<SignInScreen> {
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: _login,
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushReplacementNamed(HomePage.routeName);
+                  },
                   child: Text(
                     'Sign In',
                     style: TextStyle(color: Colors.white, fontSize: 20),
